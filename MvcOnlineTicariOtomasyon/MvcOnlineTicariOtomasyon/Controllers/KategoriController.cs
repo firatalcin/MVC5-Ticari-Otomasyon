a3420171aa942423394363e7a70781c41f4fce51
Fırat Alçın <firatalcin04@gmail.com>
2022-02-18 14:21:24 +0300

    Update

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,59 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+
+        Context c = new Context();
+        public ActionResult KategoriListele()
+        {
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("KategoriListele");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kate = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kate);
+            c.SaveChanges();
+            return RedirectToAction("KategoriListele");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriId);
+            ktgr.KategoriAdi = k.KategoriAdi;
+            c.SaveChanges();
+            return RedirectToAction("KategoriListele");
+        }
+
+
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult UrunListele()
+        {
+            var urunler = c.Uruns.Where(x => x.Durum == true).ToList();
+            return View(urunler);
+        }
+
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from x in c.Kategoris.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.KategoriAdi,
+                                                Value = x.KategoriId.ToString()
+                                            }).ToList();
+            ViewBag.dgr1 = degerler;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UrunEkle(Urun p)
+        {
+            c.Uruns.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("UrunListele");
+        }
+
+        public ActionResult UrunSil(int id)
+        {
+            var deger = c.Uruns.Find(id);
+            deger.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("UrunListele");
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = c.Uruns.Find(id);
+            List<SelectListItem> degerler = (from x in c.Kategoris.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.KategoriAdi,
+                                                 Value = x.KategoriId.ToString()
+                                             }).ToList();
+            ViewBag.dgr1 = degerler;
+            return View("UrunGetir", urun);
+        }
+
+        public ActionResult UrunGuncelle(Urun p)
+        {
+            var urn = c.Uruns.Find(p.UrunId);
+            urn.UrunAd = p.UrunAd;
+            urn.AlisFiyat = p.AlisFiyat;
+            urn.Durum = p.Durum;
+            urn.KategoriId = p.KategoriId;
+            urn.Marka = p.Marka;
+            urn.SatisFiyat = p.SatisFiyat;
+            urn.Stok = p.Stok;
+            urn.UrunGorsel = p.UrunGorsel;
+            c.SaveChanges();
+            return RedirectToAction("UrunListele");
+        }
+    }
+}
